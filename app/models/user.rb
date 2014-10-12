4ac68ecdad424edee5a5942101c915cff4a7c15e
jannisH <jannis.hoppe89@gmail.com>
2014-10-12 13:02:35 +0200

    Added the review modle

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
   validates :full_name, presence: true
+  has_many :reviews
   has_secure_password
   validates :password, on: :create, length: {minimum: 6}
   validates :email, uniqueness: true
--- app/models/video.rb
+++ app/models/video.rb
@@ -1,11 +1,16 @@
 class Video < ActiveRecord::Base
 	belongs_to :category
+	has_many :reviews
 	validates :title, presence: true
 	validates :description, presence: true
-
+	validates_uniqueness_of :review, scope: :user_id
 
 	def search_by_title(search_param)
-		string = "%" + search_param + "%"
-		Video.where("title LIKE '#{string}'")
+	  string = "%" + search_param + "%"
+	  Video.where("title LIKE '#{string}'")
+	end
+
+	def average_rating
+	  self.reviews.inject(0){|sum, review| sum+review.rating} / self.reviews.size
 	end
 end
